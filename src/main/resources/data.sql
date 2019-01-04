902909cd2fcff7c2141a69d6e9727ca8df96e300
a8282 <a8282@MDDESK296.amberroad.com>
2019-01-04 18:42:10 +0530

    changes in role & data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,8 +5,8 @@
 --alter table user_role add constraint FKa68196081fvovjhkek5m97n3y foreign key (role_id) references role
 --alter table user_role add constraint FK859n2jvi8ivhui0rl0esws6o foreign key (user_id) references user
 
-insert into role(id,name) values(1,'ADMIN');
-insert into role(id,name) values(2,'USER');
+insert into role(id,name) values(1,'ROLE_ADMIN');
+insert into role(id,name) values(2,'ROLE_USER');
 insert into user(id,password,username) values(1,'$2a$10$DL6xXzR3WzGaiJilQOcM1e0Yn.vKi6NunEKHebPidT97wrYEdE7Eu','shailendra');
 insert into user(id,password,username) values(2,'$2a$10$DL6xXzR3WzGaiJilQOcM1e0Yn.vKi6NunEKHebPidT97wrYEdE7Eu','Raj');
 insert into user_role(user_id,role_id) values(1,1);
